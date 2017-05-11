13b8f31065019de58fd76bfdf2d81acb6a525b23
JD <leonelgalizzi@gmail.com>
2017-05-11 10:26:03 -0300

    punto inicial

--- miCursadaUniversitaria.pl
+++ miCursadaUniversitaria.pl
@@ -1,1 +1,49 @@
-%holaa
+%PARTE 1
+
+%Base de Conocimiento
+%materia(Nombre,HorasTotalesCursada).
+materia(paradigmasDeProgramacion,64).
+materia(pdp,10).
+promocionable(phm).
+correlativa(matematicaI(10),matematicaII).
+correlativa(matematicaII,matematicaIII).
+
+%Las Materias
+
+%1-----------
+
+esPesada(Materia,HorasTotales):-
+	HorasTotales >100.
+	
+esPesada2(materia(Materia,HorasTotales)):-
+	HorasTotales >100.
+	
+esPesada(Materia,HorasTotales):-
+	%tieneNombreCorto(Materia),
+    not(promocionable(Materia)).
+	
+% tieneNombreCorto(Materia):-
+	% nombreConMasDeQuinceLetras(Materia).
+	
+	%atom_length
+%2------------
+
+ %forall(quiereIr(Persona, Destino), quedaEn(Destino, Zona)).
+   
+  
+materiaInicial(Materia):-
+	not(correlativa(Materia,_)).
+	
+materiasNecesariasParaCursar(Materia):-
+	correlativa(Materia,Materia2).
+materiasNecesariasParaCursar(Materia):-
+	correlativa(Materia,Materia2),
+	correlativa(Materia2,Materia3).
+
+	
+%ancestro(Persona, Ancestro):-
+%	padre(Persona, Ancestro).
+%ancestro(Persona, Ancestro):-
+%	padre(Persona, Padre),
+%	ancestro(Padre, Ancestro).	
+	
