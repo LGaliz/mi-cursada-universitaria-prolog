fd45cdb05972be8c65358f763d674e6f8168c98d
marin-h <marina@gcoop.coop>
2017-05-11 11:14:32 -0300

    Ahora si ok primer punto

--- miCursadaUniversitaria.pl
+++ miCursadaUniversitaria.pl
@@ -1,49 +1,57 @@
-%PARTE 1
-
+use_module(library(plunit)).
+load_test_files(tests.plt).
 %Base de Conocimiento
 %materia(Nombre,HorasTotalesCursada).
-materia(paradigmasDeProgramacion,64).
-materia(pdp,10).
-promocionable(phm).
-correlativa(matematicaI(10),matematicaII).
+%materia(phm,604).
+%materia(pdp,10).
+promocionable(pdp).
 correlativa(matematicaII,matematicaIII).
+
+promocionable(algoritmosI).
+promocionable(laboratorioDeComputacionI).
+promocionable(laboratorioDeComputacionII)
+promocionable(matematicaI).
+promocionable(matematicaII).
+promocionable(electricidadYMagnetismo).
+promocionable(spd).
+promocionable(sistemasOperativos).
+promocionable(pdp).
+
+% materia(algoritmosI tiene que tener mas de 100 horas
+% materia(basesDeDatos, 120).
+Las materias iniciales de la carrera son: Matemática I, Laboratorio de Computación I, Electricidad y Magnetismo.
+Las materias totales necesarias para cursar Algoritmos I son: Matemática I y II, Laboratorio de Computación I y II, Sistemas de Procesamiento de Datos.
 
 %Las Materias
 
 %1-----------
 
-esPesada(Materia,HorasTotales):-
-	HorasTotales >100.
-	
-esPesada2(materia(Materia,HorasTotales)):-
-	HorasTotales >100.
-	
-esPesada(Materia,HorasTotales):-
-	%tieneNombreCorto(Materia),
-    not(promocionable(Materia)).
-	
-% tieneNombreCorto(Materia):-
-	% nombreConMasDeQuinceLetras(Materia).
-	
-	%atom_length
-%2------------
+esPesada(materia(_,HorasTotales)):-
+	HorasTotales > 100.
+
+esPesada(materia(Nombre,_)):-
+    tieneNombreCorto(Nombre),
+    not(promocionable(Nombre)).
+
+tieneNombreCorto(Nombre):-
+	atom_length(Nombre,X),
+	X =< 15.
 
  %forall(quiereIr(Persona, Destino), quedaEn(Destino, Zona)).
-   
-  
+
+
 materiaInicial(Materia):-
 	not(correlativa(Materia,_)).
-	
+
 materiasNecesariasParaCursar(Materia):-
 	correlativa(Materia,Materia2).
 materiasNecesariasParaCursar(Materia):-
 	correlativa(Materia,Materia2),
 	correlativa(Materia2,Materia3).
 
-	
-%ancestro(Persona, Ancestro):-
-%	padre(Persona, Ancestro).
-%ancestro(Persona, Ancestro):-
-%	padre(Persona, Padre),
-%	ancestro(Padre, Ancestro).	
-	
+
+				%ancestro(Persona, Ancestro):-
+				%	padre(Persona, Ancestro).
+				%ancestro(Persona, Ancestro):-
+				%	padre(Persona, Padre),
+				%	ancestro(Padre, Ancestro).
